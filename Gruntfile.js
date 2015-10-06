2f74649fbc286b9b49366cd1c9a3819c9dd1caba
kevmo <k.james.moore@gmail.com>
2015-10-06 15:03:24 -0400

    grunt-mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,34 @@
 module.exports = function(grunt){
 
 	grunt.config.init({
+
+		pkg: grunt.file.readJSON('package.json'),
+
 		jshint: {
 			all: [
 			'Gruntfile.js',
 			'hello.js'
 			]
+		},
+
+		mocha: {
+			all: {
+				src: ['test/testrunner.html']
+			},
+			options: {
+				run: true
+			}
 		}
+
+
 	});
 
+
+
+	grunt.loadNpmTasks('grunt-mocha');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.registerTask('default', ['jshint']);
+
+
+	grunt.registerTask('default', ['jshint', 'mocha']);
 };
